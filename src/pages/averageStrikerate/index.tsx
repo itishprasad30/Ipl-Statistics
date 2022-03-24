91e9c91bb809a5b1a94a4e5c6f212748f5858467
itishprasad30 <itishprasad20@gmail.com>
2022-03-24 21:04:11 +0530

    some pages are added

---
+++ src/pages/averageStrikerate/index.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+
+import Footer from "../../components/Footer";
+
+import MostRuns from "../../components/MostRuns";
+
+import Navigation from "../../components/Navigation";
+import Header from "../../components/Header";
+
+export default function MostRunsPage({ data }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>IPL Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Header
+          content="Average Strikerate"
+          description="Most runs and average strikerate for every player"
+        />
+        <Navigation home="/" goBack="/" />
+        <MostRuns mostRunsDetails={data} />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `https://young-wildwood-83401.herokuapp.com/most_runs_average_strikerate`
+  );
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+  };
+}
